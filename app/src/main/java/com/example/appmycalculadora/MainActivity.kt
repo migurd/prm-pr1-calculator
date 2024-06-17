e7515434335d1a539e49ff94039d8decaee1b28b
Angel Qui <angelquimorita@gmail.com>
2024-06-17 16:25:59 -0700

    Se terminó el proyecto

--- app/src/main/java/com/example/appmycalculadora/MainActivity.kt
+++ app/src/main/java/com/example/appmycalculadora/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.appmycalculadora
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -13,8 +14,8 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     private lateinit var inUsuario : EditText
     private lateinit var inPassword : EditText
-    private lateinit var btnIngresar : EditText
-    private lateinit var btnSalir : EditText
+    private lateinit var btnIngresar : Button
+    private lateinit var btnSalir : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/appmycalculadora/OperacionesActivity.kt
+++ app/src/main/java/com/example/appmycalculadora/OperacionesActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -55,6 +56,7 @@ class OperacionesActivity : AppCompatActivity() {
 
         val bundle : Bundle? = intent.extras
         outUsuario.text = bundle?.getString("name")
+        operaciones = Operaciones(0f, 0f)
     }
 
     private fun validar() : Boolean {
@@ -65,10 +67,73 @@ class OperacionesActivity : AppCompatActivity() {
         return true
     }
 
+    private fun assignValues() {
+        if (validar()) {
+            operaciones.num1 = inNum1.text.toString().toFloat()
+            operaciones.num2 = inNum2.text.toString().toFloat()
+        } else {
+            throw IllegalArgumentException("Inserte valores válidos en ambos campos, por favor.")
+        }
+    }
+
+    private fun formatNumber(n : Float) : String {
+        val formattedNumber = String.format("%.2f", n)
+        return formattedNumber
+    }
+
     public fun initClickEvents() {
 
         btnSumar.setOnClickListener(View.OnClickListener {
-
+            try {
+                if (validar()) {
+                    assignValues()
+                    outResultado.text = "Resultado: " + formatNumber(operaciones.suma())
+                } else {
+                    Toast.makeText(this, "Inserte valores, por favor.", Toast.LENGTH_SHORT).show()
+                }
+            } catch(e : IllegalArgumentException) {
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+            }
+        })
+        btnRestar.setOnClickListener(View.OnClickListener {
+            try {
+                if (validar()) {
+                    assignValues()
+                    outResultado.text = "Resultado: " + formatNumber(operaciones.restar())
+                } else {
+                    Toast.makeText(this, "Inserte valores, por favor.", Toast.LENGTH_SHORT).show()
+                }
+            } catch(e : IllegalArgumentException) {
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+            }
+        })
+        btnMultiplicar.setOnClickListener(View.OnClickListener {
+            try {
+                if (validar()) {
+                    assignValues()
+                    outResultado.text = "Resultado: " + formatNumber(operaciones.multiplicar())
+                } else {
+                    Toast.makeText(this, "Inserte valores, por favor.", Toast.LENGTH_SHORT).show()
+                }
+            } catch(e : IllegalArgumentException) {
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+            }
+        })
+        btnDividir.setOnClickListener(View.OnClickListener {
+            try {
+                if (validar()) {
+                    assignValues()
+                    if (operaciones.num2.toInt() == 0) {
+                        outResultado.text = "Resultado: INDEFINIDO"
+                    } else {
+                        outResultado.text = "Resultado: " + formatNumber(operaciones.dividir())
+                    }
+                } else {
+                    Toast.makeText(this, "Inserte valores, por favor.", Toast.LENGTH_SHORT).show()
+                }
+            } catch(e : IllegalArgumentException) {
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+            }
         })
 
         btnLimpiar.setOnClickListener(View.OnClickListener {
