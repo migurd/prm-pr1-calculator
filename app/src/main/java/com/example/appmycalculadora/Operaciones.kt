cbf2df95e8a1a726da3d60845322c967aaa7e112
Angel Qui <angelquimorita@gmail.com>
2024-06-17 13:24:33 -0700

    Object Calculadora created

---
+++ app/src/main/java/com/example/appmycalculadora/Operaciones.kt
@@ -0,0 +1,17 @@
+package com.example.appmycalculadora
+
+class Operaciones(var num1: Float, var num2: Float) {
+    // var --> mutable
+    public fun suma() : Float {
+        return this.num1 + this.num2;
+    }
+    public fun restar() : Float {
+        return this.num1 - this.num2;
+    }
+    public fun multiplicar() : Float {
+        return this.num1 * this.num2;
+    }
+    public fun dividir() : Float {
+        return this.num1 / this.num2;
+    }
+}
